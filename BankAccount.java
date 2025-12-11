6691a01b262e26d717c6d9b91f57c19c09d6c1c8
abhi-7867 <abhisekasinha43@gmail.com>
2025-12-11 14:22:10 +0530

    feat(account): introduce BankAccount model skeleton with account number and balance

---
+++ BankAccount.java
@@ -0,0 +1,9 @@
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+}
