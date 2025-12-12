2c4bf5cd477232f64990838fe5035a2138e67d84
abhi-7867 <abhisekasinha43@gmail.com>
2025-12-12 11:05:45 +0530

    feat(account): add PIN field and verifyPin method stub

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,15 @@
 public class BankAccount {
     private String accountNumber;
+    private int pin;
     private double balance;
     
-    public BankAccount(String accountNumber, double initialBalance) {
+    public BankAccount(String accountNumber, int pin, double initialBalance) {
         this.accountNumber = accountNumber;
+        this.pin = pin;
         this.balance = initialBalance;
+    }
+    
+    public boolean verifyPin(int enteredPin) {
+        return this.pin == enteredPin;
     }
 }
