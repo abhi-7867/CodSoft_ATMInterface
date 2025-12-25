c165e46c7ba50f477aab3de495465bb2fe2892f0
abhi-7867 <abhisekasinha43@gmail.com>
2025-12-25 15:30:00 +0530

    feat(transaction): add timestamp field using java.time.LocalDateTime

--- Transaction.java
+++ Transaction.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+
 public class Transaction {
     private String type;
     private double amount;
     private double balanceAfter;
+    private LocalDateTime timestamp;
 }
