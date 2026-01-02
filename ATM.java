30c825e7c771f197fe782a9f53e86c015df6049d
abhi-7867 <abhisekasinha43@gmail.com>
2026-01-02 15:45:30 +0530

    feat(atm): implement user authentication and session management in ATM

--- ATM.java
+++ ATM.java
@@ -1,3 +1,25 @@
 public class ATM {
     private BankAccount currentAccount;
+    
+    public boolean authenticate(BankAccount account, int pin) {
+        if (account == null) {
+            return false;
+        }
+        if (account.isLocked()) {
+            return false;
+        }
+        boolean isValid = account.verifyPin(pin);
+        if (isValid) {
+            this.currentAccount = account;
+        }
+        return isValid;
+    }
+    
+    public void logout() {
+        currentAccount = null;
+    }
+    
+    public boolean isAuthenticated() {
+        return currentAccount != null;
+    }
 }
