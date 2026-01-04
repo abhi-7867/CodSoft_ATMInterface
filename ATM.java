4ec734f2027a987407048bf385675e93f611ced7
abhi-7867 <abhisekasinha43@gmail.com>
2026-01-04 16:30:20 +0530

    feat(atm): define deposit threshold constants

--- ATM.java
+++ ATM.java
@@ -2,6 +2,8 @@ public class ATM {
     private BankAccount currentAccount;
     private static final double MIN_WITHDRAWAL = 10.0;
     private static final double MAX_WITHDRAWAL = 5000.0;
+    private static final double MIN_DEPOSIT = 5.0;
+    private static final double MAX_DEPOSIT = 10000.0;
     
     public boolean authenticate(BankAccount account, int pin) {
         if (account == null) {
